15fea405fc349ce39bd0fb48c96ffdf0f17b902b
Igune Justine Robert <igunejustinerobert@gmail.com>
2020-12-01 19:46:55 +0300

    Modified app.js

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 function FizzBuzz(n) {
     
-  for (i = 1; i <= n; i++) {
-  if (i%15 === 0) {
-  console.log ("FizzBuzz");
+  for (let i = 1; i <= n; i++) {
+  if (i%3 === 0) {
+  console.log ("Fizz");
 }       
-  else if (i%3 === 0) {
-  console.log ("Fizz"); 
-}
   else if (i%5 === 0) {
-  console.log("Buzz");
+  console.log ("Buzz"); 
 }
+  else if (i%5 === 0 && i%3 ===0) {
+  console.log("FizzBuzz");
+}
+//else if (i%5 === 0 || i%3) {
+  //console.log(FizzBuzz);
+//}
           
   else {
   console.log(i);
